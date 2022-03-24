77d76d4932c05d402f6d526a7292bd15ddaa90ba
Gustavo Rosolen Brigatti <gustavo.brigatti13@gmail.com>
2022-03-24 16:36:42 -0300

    teste de listagem de todos os carros da primeira página

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -4,23 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Carro;
 use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 class TesteController
 {
     public function index(Client $client){
         $crawler = $client->request('GET', 'https://olx.com.br/autos-e-pecas/carros-vans-e-utilitarios?q=onix');
-        $inlineCarClass = 'sc-1fcmfeb-2 fvbmlV';
-        $crawler->filter("[class='$inlineCarClass']")->each(function ($carNode){
-            $carro = new Carro();
+        $crawler->filter("[class='sc-1fcmfeb-2 fvbmlV']")->each(function (Crawler $carNode){
+            $carro = [];
             $nameNode = $carNode->filter("[class='sc-1mbetcw-0 fKteoJ sc-ifAKCX jyXVpA']")->first();
             $infosNode = $carNode->filter("[class='sc-1j5op1p-0 lnqdIU sc-ifAKCX eLPYJb']")->first();
             $priceNode = $carNode->filter("[class='sc-ifAKCX eoKYee']")->first();
             $localNode = $carNode->filter("[class='sc-7l84qu-1 ciykCV sc-ifAKCX dpURtf']")->first();
-            $carro->nome = $nameNode->text();
-            $carro->infos = $infosNode->text();
-            $carro->preco = $priceNode->text();
-            $carro->local = $localNode->text();
-            dd($carro);
+            try{
+                $carro['name'] = $nameNode->text();
+                $carro['infos'] = $infosNode->text();
+                $carro['price'] = $priceNode->text();
+                $carro['local'] = $localNode->text();
+            }catch (\Exception $e){
+
+            }
+            var_dump($carro);
         });
     }
 }
