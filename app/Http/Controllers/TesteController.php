ee0c93ddf096c69dabe5bacc54c6f1d74326f521
Gustavo Rosolen Brigatti <gustavo.brigatti13@gmail.com>
2022-03-28 14:34:03 -0300

    adição da atualização do arquivo cache caso tenha passado mais de uma hora desde sua criação

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -17,32 +17,51 @@ class TesteController
     public function store(Request $request, Client $client){
         //Caso não exista o cache, ele executa o código e cria, se existir ele apenas executa o cache já criado
         if (!file_exists('Carros/'.$request->carro.'.html')){
-            //Início do cache
-            ob_start();
-            // Para teste estou pegando apenas os resultados das duas primeiras páginas
-            // porém a olx lista da página 1-100, casso queira toda a listagem podemos
-            // apenas alterar para $<=100 na validação do for
-            for ($i=1; $i<=2; $i++){
-                if ($i==1){
-                    $crawler = $client->request('GET', "https://olx.com.br/autos-e-pecas/carros-vans-e-utilitarios?q='.$request->carro.'");
-                }else{
-                    $crawler = $client->request('GET', "https://olx.com.br/autos-e-pecas/carros-vans-e-utilitarios?o='.$i.'&q='.$request->carro.'");
-                }
-                $this->extractContactsFrom($crawler, $i);
-            }
-            echo $this->mensagem;
-            //Obtenção do código do cache
-            $cache = ob_get_contents();
-            //Final do cache
-            ob_end_clean();
-            //Cache salvo para busca do modelo específico
-            file_put_contents('Carros/'.$request->carro.'.html', $cache);
-            //Exibição do cache que acabou de ser interpretado e criado
-            echo $cache;
+            $this->criar_cache($request, $client);
         }else{
-            //Exibição do cache já interpretado anteriormente
-            require('Carros/'.$request->carro.'.html');
+            //Obtenção da data atual
+            $data_atual = new \DateTime();
+            //Obtenção da data de criação do arquivo cache
+            $data_cache = new \DateTime(date('Y-m-d H:i:s', filectime('Carros/'.$request->carro.'.html')));
+            //Obtenção da diferença de horas da data atual para a data de criação do arquivo cache
+            $diff = $data_atual->diff($data_cache);
+            $horas = $diff->h + ($diff->days * 24);
+            //Se o arquivo foi criado a mais de uma hora, o código é interpretado novamente e armazenado em um cache
+            if ($horas>=1){
+                //Exclusão do arquivo criado anteriormente
+                unlink('Carros/'.$request->carro.'.html');
+                $this->criar_cache($request, $client);
+            }else{
+                //Exibição do cache já interpretado anteriormente
+                require('Carros/'.$request->carro.'.html');
+            }
         }
+    }
+
+    public function criar_cache($request, $client){
+        //Início do cache
+        ob_start();
+        // Para teste estou pegando apenas os resultados das duas primeiras páginas
+        // porém a olx lista da página 1-100, casso queira toda a listagem podemos
+        // apenas alterar para $<=100 na validação do for
+        $this->mensagem .= '<h2>MODELO: '.$request->carro.'</h2>';
+        for ($i=1; $i<=2; $i++){
+            if ($i==1){
+                $crawler = $client->request('GET', "https://olx.com.br/autos-e-pecas/carros-vans-e-utilitarios?q='.$request->carro.'");
+            }else{
+                $crawler = $client->request('GET', "https://olx.com.br/autos-e-pecas/carros-vans-e-utilitarios?o='.$i.'&q='.$request->carro.'");
+            }
+            $this->extractContactsFrom($crawler, $i);
+        }
+        echo $this->mensagem;
+        //Obtenção do código do cache
+        $cache = ob_get_contents();
+        //Final do cache
+        ob_end_clean();
+        //Cache salvo para busca do modelo específico
+        file_put_contents('Carros/'.$request->carro.'.html', $cache);
+        //Exibição do cache que acabou de ser interpretado e criado
+        echo $cache;
     }
 
     public function extractContactsFrom(Crawler $crawler, $page){
