73edac14858891573674df59ca8b0ba5245b2843
Gustavo Rosolen Brigatti <gustavo.brigatti13@gmail.com>
2022-03-28 14:46:31 -0300

    finalização do projeto com algumas edições na index.blade.php

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -14,42 +14,42 @@ class TesteController
         return view('index');
     }
 
-    public function store(Request $request, Client $client){
+    public function show($carro, Client $client){
         //Caso não exista o cache, ele executa o código e cria, se existir ele apenas executa o cache já criado
-        if (!file_exists('Carros/'.$request->carro.'.html')){
-            $this->criar_cache($request, $client);
+        if (!file_exists('Carros/'.$carro.'.html')){
+            $this->criar_cache($carro, $client);
         }else{
             //Obtenção da data atual
             $data_atual = new \DateTime();
             //Obtenção da data de criação do arquivo cache
-            $data_cache = new \DateTime(date('Y-m-d H:i:s', filectime('Carros/'.$request->carro.'.html')));
+            $data_cache = new \DateTime(date('Y-m-d H:i:s', filectime('Carros/'.$carro.'.html')));
             //Obtenção da diferença de horas da data atual para a data de criação do arquivo cache
             $diff = $data_atual->diff($data_cache);
             $horas = $diff->h + ($diff->days * 24);
             //Se o arquivo foi criado a mais de uma hora, o código é interpretado novamente e armazenado em um cache
             if ($horas>=1){
                 //Exclusão do arquivo criado anteriormente
-                unlink('Carros/'.$request->carro.'.html');
-                $this->criar_cache($request, $client);
+                unlink('Carros/'.$carro.'.html');
+                $this->criar_cache($carro, $client);
             }else{
                 //Exibição do cache já interpretado anteriormente
-                require('Carros/'.$request->carro.'.html');
+                require('Carros/'.$carro.'.html');
             }
         }
     }
 
-    public function criar_cache($request, $client){
+    public function criar_cache($carro, $client){
         //Início do cache
         ob_start();
         // Para teste estou pegando apenas os resultados das duas primeiras páginas
         // porém a olx lista da página 1-100, casso queira toda a listagem podemos
         // apenas alterar para $<=100 na validação do for
-        $this->mensagem .= '<h2>MODELO: '.$request->carro.'</h2>';
+        $this->mensagem .= '<h2>MODELO DO CARRO: '.$carro.'</h2>';
         for ($i=1; $i<=2; $i++){
             if ($i==1){
-                $crawler = $client->request('GET', "https://olx.com.br/autos-e-pecas/carros-vans-e-utilitarios?q='.$request->carro.'");
+                $crawler = $client->request('GET', "https://olx.com.br/autos-e-pecas/carros-vans-e-utilitarios?q='.$carro.'");
             }else{
-                $crawler = $client->request('GET', "https://olx.com.br/autos-e-pecas/carros-vans-e-utilitarios?o='.$i.'&q='.$request->carro.'");
+                $crawler = $client->request('GET', "https://olx.com.br/autos-e-pecas/carros-vans-e-utilitarios?o='.$i.'&q='.$carro.'");
             }
             $this->extractContactsFrom($crawler, $i);
         }
@@ -59,7 +59,7 @@ class TesteController
         //Final do cache
         ob_end_clean();
         //Cache salvo para busca do modelo específico
-        file_put_contents('Carros/'.$request->carro.'.html', $cache);
+        file_put_contents('Carros/'.$carro.'.html', $cache);
         //Exibição do cache que acabou de ser interpretado e criado
         echo $cache;
     }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,11 +2,21 @@
 <html>
 <body>
 
-<form action="{{ route('teste.store') }}" method="post">
-    {{ csrf_field() }}
-    <input type="text" name="carro" style="width: 300px; height: 50px; font-size: 25px" placeholder="Digite o modelo do carro"/>
-    <button style="height: 50px; width: 100px" type="submit">BUSCAR</button>
-</form>
+<input type="text" name="carro" id="carro" style="width: 300px; height: 50px; font-size: 25px" placeholder="Digite o modelo do carro"/>
+<button style="height: 50px; width: 100px" id="btnSubmit" type="button" onclick="clickButton()">BUSCAR</button>
 
+<script>
+    var input = document.getElementById("carro");
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("btnSubmit").click();
+        }
+    });
+    function clickButton() {
+        var carro = document.getElementById('carro').value;
+        window.location.href = "/teste/"+carro;
+    }
+</script>
 </body>
 </html>
